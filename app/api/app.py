819a9da7a042a19a9c99bfbee4146def91619e64
Reynold97 <reynoldalejandro46@gmail.com>
2024-09-17 21:37:37 -0400

    new structure

--- app/api/app.py
+++ app/api/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-from data_ops.download import router as download_router
+from app.core.loaders.gdrive_loader import router as download_router
 
 load_dotenv()
 
